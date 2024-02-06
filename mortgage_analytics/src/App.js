e714452c3340386f44d0c3f990dfe22b6b59d18f
Steve Thomas <steve13thomas@gmail.com>
2024-02-06 05:36:50 +0000

    updates routes

--- mortgage_analytics/src/App.js
+++ mortgage_analytics/src/App.js
@@ -1,20 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Navbar from './components/Navbar'
+import Home from './pages/Home'
 import About from './pages/About';
 import Login from "./pages/Login";
-import Login from "./pages/Data";
+import Data from "./pages/Data";
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Navbar />}>
+      {/* <Route path="/" element={<Navbar />}> */}
         <Route index element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="login" element={<Login />} />
         <Route path="data" element={<Data />} />
-      </Route>
+      {/* </Route> */}
     </Routes>
     </BrowserRouter>
   );
