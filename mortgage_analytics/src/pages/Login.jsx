f82e64a0fe49e4cb79c0a60270568d5c29d167ad
alim5 <alim5@tcd.ie>
2024-02-13 14:46:04 +0000

    completed Log in page

--- mortgage_analytics/src/pages/Login.jsx
+++ mortgage_analytics/src/pages/Login.jsx
@@ -1,7 +1,31 @@
+import '../css/Login.css';
+import Form from 'react-bootstrap/Form';
 const Login = () => {
     return (
-        <div>
-            <p>Welcome to Login page!!</p>
+        <div className="login-container">
+            <h6>Welcome Back</h6>
+            <h4>Sign in to your account</h4>
+
+            <Form>
+                <Form.Group className="mb-3" controlId="formGroupEmail">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formGroupPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" />
+                </Form.Group>
+                <button type="submit" className="btn btn-primary">Continue</button>
+            </Form>
+
+            <p className="forgot-password">
+                <a href="#">Forgot your password?</a> 
+            </p>
+
+            <p className="sign-up">
+            Don't have an account?   <a href="#">Sign up</a> 
+            </p>
+
         </div>
     );
 };
