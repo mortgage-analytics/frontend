3d8128f953662b66b475888bb4d5860568402314
Nehal <batran@tcd.ie>
2024-02-21 14:06:30 +0000

    added a sample chart from guide

--- mortgage_analytics/src/pages/Data.jsx
+++ mortgage_analytics/src/pages/Data.jsx
@@ -1,6 +1,34 @@
+import React, { PureComponent } from 'react';
+import { render } from "react-dom";
+import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
+
+const data01 = [
+  { name: 'Group A', value: 400 },
+  { name: 'Group B', value: 300 },
+  { name: 'Group C', value: 300 },
+  { name: 'Group D', value: 200 },
+];
+const data02 = [
+  { name: 'A1', value: 100 },
+  { name: 'A2', value: 300 },
+  { name: 'B1', value: 100 },
+  { name: 'B2', value: 80 },
+  { name: 'B3', value: 40 },
+  { name: 'B4', value: 30 },
+  { name: 'B5', value: 50 },
+  { name: 'C1', value: 100 },
+  { name: 'C2', value: 200 },
+  { name: 'D1', value: 150 },
+  { name: 'D2', value: 50 },
+];
+
 const Data = () => {
   return (
     <div>
+      <PieChart width={400} height={400}>
+        <Pie data={data01} dataKey="value" cx="50%" cy="50%" outerRadius={60} fill="#8884d8" />
+        <Pie data={data02} dataKey="value" cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#82ca9d" label />
+      </PieChart>
 
       <header>
         <h1 style={{ textAlign: "centre" }}>Bottlenecks</h1>
@@ -27,9 +55,12 @@ const Data = () => {
           <li>Stringent Underwriting Standards</li>
         </ul>
       </main>
-
     </div>
+
   );
 };
 
 export default Data;
+
+
+
