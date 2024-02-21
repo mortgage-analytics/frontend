d2845719d42c749093b4a1e02123db37675cbb2f
Nehal <batran@tcd.ie>
2024-02-21 14:54:08 +0000

    inner circle complete

--- mortgage_analytics/src/pages/Data.jsx
+++ mortgage_analytics/src/pages/Data.jsx
@@ -6,24 +6,23 @@ import { render } from "react-dom";
 import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
 
 const data01 = [
-  { name: 'Group A', value: 400 },
-  { name: 'Group B', value: 300 },
-  { name: 'Group C', value: 300 },
-  { name: 'Group D', value: 200 },
+  { name: 'Group A', value: 300 },
+  { name: 'Group B', value: 700 },
+  { name: 'Group C', value: 500 },
 ];
 const data02 = [
   { name: 'A1', value: 100 },
   { name: 'A2', value: 300 },
-  { name: 'B1', value: 100 },
-  { name: 'B2', value: 80 },
-  { name: 'B3', value: 40 },
-  { name: 'B4', value: 30 },
-  { name: 'B5', value: 50 },
+  { name: 'A3', value: 100 },
+  { name: 'B1', value: 80 },
+  { name: 'B2', value: 40 },
+  { name: 'B3', value: 30 },
   { name: 'C1', value: 100 },
   { name: 'C2', value: 200 },
-  { name: 'D1', value: 150 },
-  { name: 'D2', value: 50 },
+  { name: 'C3', value: 150 },
+  { name: 'C4', value: 50 },
 ];
+const COLORS = ['#FF0000', '#00C49F', '#FFBB28', '#FF8042'];
 
 const Data = () => {
   return (
@@ -59,7 +58,11 @@ const Data = () => {
           </Col>
           <Col>
             <PieChart width={400} height={400}>
-              <Pie data={data01} dataKey="value" cx="50%" cy="50%" outerRadius={60} fill="#8884d8" />
+              <Pie data={data01} dataKey="value" cx="50%" cy="50%" outerRadius={60} fill="#8884d8">          
+                {data01.map((entry, index) => (
+                  <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                  ))}
+              </Pie> 
               <Pie data={data02} dataKey="value" cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#82ca9d" label />
             </PieChart>
           </Col>  
